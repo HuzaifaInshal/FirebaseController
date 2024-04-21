18aba0fd01c7be84404e5fa41928b29a9afd5c8a
Huzaifa Inshal <huzaifainshal@gmail.com>
2024-04-22 02:10:15 +0500

    'done'

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
   return (
     <div>
-      Login
+      <Link to='/main'>Main</Link>
     </div>
   )
 }
